ff2f625e78f935154d17d4f17892f899b36aa812
jmhockham <jmhockham@gmail.com>
2017-09-12 15:55:10 +0100

    Empty classes, for getting data from API end points

---
+++ src/main/scala/com/jmhockham/analytics/download/DataFilesDownloader.scala
@@ -0,0 +1,8 @@
+package com.jmhockham.analytics.download
+
+/**
+  * Created by jmhockham on 12/09/17.
+  */
+class DataFilesDownloader {
+  //TODO get data from the gov API links
+}
---
+++ src/test/scala/com/jmhockham/analytics/download/DataFilesDownloaderTest.java
@@ -0,0 +1,9 @@
+package com.jmhockham.analytics.download;
+
+
+/**
+ * Created by jmhockham on 12/09/17.
+ */
+public class DataFilesDownloaderTest {
+    //TODO test the API links, and that the data is in an expected format
+}
